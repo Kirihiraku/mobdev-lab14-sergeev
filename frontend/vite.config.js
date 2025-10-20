16199db9af8dd02b6b96900249cc90239052f631
Kirihiraku <vladser.sergeev2017@yandex.ru>
2025-10-20 06:20:24 +0000

    lab14

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from '@vite-pwa/vite-plugin'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,
+            },
+            manifestFilename: 'manifest.json',
+            includeManifestIcons: false,
+        }),
+    ],
 })
